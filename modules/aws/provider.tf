c999739bd5f77df858adf17f0df064f9779430f3
szczyppka <p.szczypka.dev@gmail.com>
2026-07-29 18:32:12 +0200

    infra: migrate to S3 remote state with native locking

--- modules/aws/provider.tf
+++ modules/aws/provider.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket       = "szczypka-web-tfstate-997"
+    key          = "aws/terraform.tfstate"
+    region       = "eu-central-1"
+    encrypt      = true
+    use_lockfile = true
+  }
 }
 
 provider "aws" {
